2aa21fb07acc05adb60ed736208675a59414cf0e
daFinndus <finnlucajensen555@gmail.com>
2024-12-06 16:52:39 +0100

    feat: Update script

--- del-empty-folders.py
+++ del-empty-folders.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 import argparse
-from pathlib import Path
+
 
 def create_arg_parser():
     # Creates and returns the ArgumentParser object
@@ -11,29 +11,37 @@ def create_arg_parser():
     parser.add_argument("path", help="Path to check for empty folders")
     return parser
 
+
 def get_folders(path):
     # Returns a list of folders in the provided path
     folders = []
 
-    print("Checking for folders in: ", path)
+    print("Checking for empty folders in: ", path)
 
-    for folder in os.listdir(path):
-        folder_path = os.path.join(path, folder)
-        if os.path.isdir(folder_path):
-            folders.append(folder_path)
+    try:
+        for folder in os.listdir(path):
+            folder_path = os.path.join(path, folder)
+            if os.path.isdir(folder_path) and not os.listdir(folder_path):
+                folders.append(folder_path)
+                print("Found empty folders: ", folders)
+    except PermissionError:
+        print("Permission denied for a folder!")
+    except Exception as e:
+        print("An error occurred: ", e)
 
-    print("Found folders: ", folders)
     return folders
 
-def delete_empty_folders(folders):
-    # Deletes empty folders from the provided list
-    for folder in folders:
-        if not os.listdir(folder):
-            confirmation = input(f"Do you want to delete {folder}? [y/n] ").lower()
 
+def delete_empty_folders(folders):
+    if folders:
+        # Deletes folders from the provided list
+        for folder in folders:
+            confirmation = input(f"Do you want to delete {folder}? [y/n] ").lower()
             if confirmation == "y" or confirmation == "yes":
                 os.rmdir(folder)
                 print(f"{folder} has been deleted.")
+    else:
+        print("No empty folders found.")
 
 
 if __name__ == "__main__":
@@ -42,10 +50,7 @@ if __name__ == "__main__":
     if os.path.exists(parsed_args.path):
         print("Path exists, continuing...")
 
-        get_folders(parsed_args.path)
         delete_empty_folders(get_folders(parsed_args.path))
     else:
         print("Path does not exist")
         sys.exit(1)
-
-
